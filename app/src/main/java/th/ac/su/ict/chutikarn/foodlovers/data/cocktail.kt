df796b8aaff21cb9c1c4bba452acb121479d7567
Benz1905 <Chutikarn.D@users.noreply.github.com>
2020-03-09 21:54:31 +0700

    uppppppp

---
+++ app/src/main/java/th/ac/su/ict/chutikarn/foodlovers/data/cocktail.kt
@@ -0,0 +1,10 @@
+package th.ac.su.ict.chutikarn.foodlovers.data
+
+
+ data class cocktail (
+val cocktailFile: String,
+val cocktailName: String,
+val price: String,
+val place: String,
+val description: String
+ )
